ba21e756609c9aaf9d1a457e84ca409293504c79
ksaa189 <hanguoxiang@zz-MAC.local>
2015-10-15 14:47:48 +0800

    添加包含文件

---
+++ ZZDevelopArchitecture/ZZDevelopArchitecture/ZZDevelopArchitecture.h
@@ -0,0 +1,19 @@
+//
+//  ZZDevelopArchitecture.h
+//  ZZDevelopArchitecture
+//
+//  Created by Zhouzheng on 15/10/15.
+//  Copyright © 2015年 ZZ. All rights reserved.
+//
+
+#ifndef ZZDevelopArchitecture_h
+#define ZZDevelopArchitecture_h
+
+#import "ZZQuick_Predefine.h"   // 预编译
+
+#import "ZZQuick_Core.h"        // 基础类
+#import "ZZQuick_UI.h"          // 界面
+#import "ZZQuick_Event.h"       // 事件
+
+
+#endif /* ZZDevelopArchitecture_h */
